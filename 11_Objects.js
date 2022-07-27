72415ab9938534f4c77219ddb38b45dca6fad1fd
Raj Randive <randiveraj96@gmail.com>
2022-07-27 23:57:15 +0530

    Passed in first trial.

---
+++ 11_Objects.js
@@ -0,0 +1,18 @@
+
+let houseKeeper1 = {
+    yearsOfExperience: 12,
+    name: "Jane Hopper",
+    cleaningRepertoire: ["bathroom", "lobby", "bedroom"]
+}
+
+console.log(houseKeeper1.name);
+console.log(houseKeeper1.yearsOfExperience);
+console.log("\n");
+console.log(houseKeeper1);
+
+
+// Now what if we want to add many other housekeeper like --- housekeeper2, housekeeper3 etc..
+
+// Then that will be very tidious work because every-time we have to specify all the properties again and again...
+
+// So for that we use a CONSTRUCTOR FUNCTION in this of Situation....!!
